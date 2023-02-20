f41169fe00cdcacbfd46f40df9d68e61187ff75b
chlip1 <lukasz.chlipala3@gmail.com>
2023-02-20 21:48:09 +0100

    api finished

--- api/index.js
+++ api/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const config = require('./config')
 const apiRouter = require('./routes/api')
+const bodyParser = require('body-parser')
 
 
 //db
 require('./db/mongoose');
-// routes
 
-app.use('/', apiRouter);
+//parser
+app.use(bodyParser.json());
+
+// routes
+app.use('/api/', apiRouter);
 
 app.listen(config.port, function() {
     console.log("serwer slucha... ")
--- api/routes/api.js
+++ api/routes/api.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
-const notesActions = require('../controllers/api/notes')
+const notesActions = require('../controllers/api/noteController')
 
-router.get('/', notesActions.saveNote)
+router.get('/notes', notesActions.getAllNotes)
+router.get('/notes/:id', notesActions.getNote)
+router.post('/notes', notesActions.saveNote)
+router.put('/notes/:id', notesActions.updateNote)
+router.delete('/notes/:id', notesActions.deleteNote)
 
 module.exports = router;
