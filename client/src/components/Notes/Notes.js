92f25c59f3748de14c8ac5b72401aae9be7904aa
chlip1 <lukasz.chlipala3@gmail.com>
2023-02-21 15:31:20 +0100

    connect frontend to api

--- client/src/components/Notes/Notes.js
+++ client/src/components/Notes/Notes.js
@@ -1,34 +1,111 @@
 import React from "react";
 import './Notes.css'
 import Note from './Note/Note'
+import NewNote from "./NewNote/NewNote";
+import Modal from "react-modal"
+import EditNote from "./EditNote/EditNote";
+import axios from '../../axios'
 
 class Notes extends React.Component {
     constructor(props){
         super(props);
-        this.notes = [
-            {
-                id: '2323',
-                title: 'Wykąpać psa',
-                body: 'Pamiętaj, aby wykąpać psa specjalnym szamponem'
-            },
-            {
-                id: '2323',
-                title: 'Wykąpać psa',
-                body: 'Pamiętaj, aby wykąpać psa specjalnym szamponem'
-            }
-        ];
+        this.state = {
+            notes: [
+                {
+                    id: '2323',
+                    title: 'Wykąpać psa',
+                    body: 'Pamiętaj, aby wykąpać psa specjalnym szamponem'
+                },
+                {
+                    id: '2333',
+                    title: 'Wykąpać psa',
+                    body: 'Pamiętaj, aby wykąpać psa specjalnym szamponem'
+                }
+            ],
+            showEditModal: false,
+            editNote: {}
+        };
     }
+
+    componentDidMount() {
+        this.fetchNotes();
+    }
+
+    async fetchNotes() {
+        const res = await axios.get('/notes');
+        const notes = res.data;
+
+        this.setState({ notes })
+        console.log(res);
+    }
+
+    async deleteNote(id) {
+        const notes = [...this.state.notes]
+            .filter(note => note._id !== id);
+            
+        await axios.delete('/notes/' + id)
+        this.setState({ notes: notes });
+    }
+
+    async addNote(note) {
+        const notes = [...this.state.notes];
+        // add to backend
+        const res = await axios.post('/notes', note)
+        const newNote = res.data;
+        // add to frontend
+        notes.push(newNote);
+        this.setState({ notes })
+    }
+
+    async editNote(note) {
+        // edit backend
+        await axios.put('/notes/' + note._id, note)
+        // edit frontend
+        const notes = [...this.state.notes];
+        const index = notes.findIndex(x => x._id === note._id);
+        if (index >= 0) {
+            notes[index] = note;
+            this.setState({ notes })
+        }
+        this.toggleModal();
+    }
+
+    toggleModal() {
+        this.setState({ 
+            showEditModal: !this.state.showEditModal })
+    }
+
+    editNoteHandler(note) {
+        this.toggleModal();
+        this.setState({ editNote: note });
+      }
+
     render() {
         return (
             <div>
                 <p>Moje notatki</p>
-                {this.notes.map(note => {
+                <NewNote
+                    onAdd={(note) => this.addNote(note)}
+                />
+                <Modal
+                    isOpen={this.state.showEditModal}
+                    contentLabel="Edytuj notatkę" >
+                        <EditNote
+                            title={this.state.editNote.title}
+                            body={this.state.editNote.body}
+                            id={this.state.editNote._id}
+                            onEdit={note => this.editNote(note)}/>
+                            <button onClick={() => this.toggleModal()}></button>
+                </Modal>
+                {this.state.notes.map(note => {
                     return (
                         <Note 
+                            key={note._id}
                             title={note.title} 
                             body={note.body}
-                            id={note.id}
-                            />
+                            id={note._id}
+                            onEdit={(note) => this.editNoteHandler(note)}
+                            onDelete={(id) => this.deleteNote(id)}/>
                     )})}
             </div>
         );
--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3001,
     database: process.env.DATABASE || "mongodb+srv://chlip1:chlip1mongoose@notes.5kcce1w.mongodb.net/?retryWrites=true&w=majority"
 };
